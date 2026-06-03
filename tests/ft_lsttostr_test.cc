09f2d1ace3384fde95f323df3aaa4e71a40f269e
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-03 16:42:47 -0300

    tests: ft_lsttostr should convert a non empty list to its string representation

--- tests/ft_lsttostr_test.cc
+++ tests/ft_lsttostr_test.cc
@@ -13,3 +13,25 @@ TEST(FTlsttostr, itShouldConvertAnEmptyListToEmptyString) {
 
 	free(result);
 }
+
+TEST(FTlsttostr, itShouldConvertANonEmptyListToItsStringRepresentation) {
+	t_list	*head = NULL;
+	ft_lstadd_back(&head, 'h');
+	t_list	*tail = head;
+	ft_lstadd_back(&tail, 'e');
+	ft_lstadd_back(&tail, 'l');
+	ft_lstadd_back(&tail, 'l');
+	ft_lstadd_back(&tail, 'o');
+
+	char *result = ft_lsttostr(head);
+
+	ASSERT_STREQ("hello", result);
+
+	free(result);
+	while (head)
+	{
+		t_list *tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
