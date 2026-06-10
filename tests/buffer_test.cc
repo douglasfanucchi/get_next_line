3a26a053a8eebdf4da155afecca0d184757d8cd4
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-06-10 16:56:35 -0300

    tests: it should fill buffer with bytes from specified file descriptor and free it

--- tests/buffer_test.cc
+++ tests/buffer_test.cc
@@ -2,6 +2,8 @@
 
 extern "C" {
 	#include <get_next_line.h>
+	#include <fcntl.h>
+	#include <math.h>
 }
 
 TEST(Buffer, itShouldInitializeBufferWithSpecificFileDescriptor) {
@@ -10,4 +12,23 @@ TEST(Buffer, itShouldInitializeBufferWithSpecificFileDescriptor) {
 	initialize_buffer(&buffer, fd);
 
 	ASSERT_TRUE(buffer.is_empty(buffer));
+}
+
+TEST(Buffer, itShouldFillBufferWithBytesFromSpecifiedFdAndFreeIt) {
+	t_buffer buffer;
+	int fd[2];
+	int expected = fmin(11, BUFFER_SIZE);
+	pipe2(fd, O_NONBLOCK);
+	write(fd[1], "hello world", 11);
+	initialize_buffer(&buffer, fd[0]);
+
+	int result = buffer.fill(&buffer);
+	ASSERT_EQ(expected, result);
+	ASSERT_FALSE(buffer.is_empty(buffer));
+
+	buffer.free(&buffer);
+	ASSERT_TRUE(buffer.is_empty(buffer));
+
+	close(fd[0]);
+	close(fd[1]);
 }
